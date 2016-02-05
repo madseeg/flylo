5388584e934520878b48579acf4f9388408f1beb
Utmost excellent mobile reception <zerilos@yahoo.com>
2016-02-05 01:11:14 +0000

    Added username column, user profile pages, and username sign-up form.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   
   # Define routes for Pages
   get '/home' => 'pages#home'
-  get '/profile' => 'pages#profile'
+  get '/user/:id' => 'pages#profile'
   get '/explore' => 'pages#explore'
   
   
